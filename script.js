c3b7f4713441bbcb50c906b191facc757801949e
patorrad <paolo.a.torrado@gmail.com>
2019-12-06 17:48:02 -0800

    Added js for follow up questions, timer render, and score input

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ var titleBox = document.querySelector("#title-box");
 var bodyBox = document.querySelector("#body-box");
 var timer = document.querySelector("#timer");
 var buttons = document.querySelector("#box2");
+var questionsBtn = document.querySelector("#btn-box");
+var inputBox = document.querySelector("#input-box");
 var questions = [
     {
       title: "Commonly used data types DO NOT include:",
@@ -32,58 +34,122 @@ var questions = [
     {
       title: "What type of animal shirt did Joe wear on Wednesday ____.",
       choices: ["Kramer", "Huskies", "Cats", "Wookies"],
-      answer: "parentheses"
+      answer: "Cats"
     },
 ];
 var timerInterval;
 
-var secondsLeft = 10;
-timer.textContent = secondsLeft;
+var secondsLeft = 600; //100th seconds
+var penalty = 100; //seconds
+var index = 0; //Question Number
+renderTimeToPage();
 timer.style = "font-size: xx-large"
 
-function deleteButtons() {
-    buttons.querySelectorAll('*').forEach(n => n.remove());
+function renderTimeToPage() {
+    var time = secondsLeft / 10;
+    timer.textContent = "Time: " + (secondsLeft % 10 === 0? time + ".0" : time);
 }
 
-function createButtons (butnChoices, parent) {
-    for (choice in butnChoices) { 
+function renderQuestions() {
+    titleBox.textContent = questions[index].title;
+    bodyBox.textContent = "";
+}
+
+function renderScore() {
+    titleBox.textContent = "Your score was " + (secondsLeft / 10).toFixed();
+    var inputB = document.createElement("input");
+    inputB.textContent = "adsf";
+    inputB.setAttribute("type", "text");
+    inputB.setAttribute("value", "Enter Name");
+    inputBox.appendChild(inputB);
+}
+
+function deleteButtons(element) {
+    element.querySelectorAll('*').forEach(n => n.remove());
+}
+
+function createButtons (btnChoices, parent) {    
+    for (choice in btnChoices) { 
     var btn = document.createElement("button"); 
-    btn.textContent = butnChoices[choice];                 
+    btn.textContent = btnChoices[choice]; 
+    btn.setAttribute("id", btnChoices[choice]);                
     parent.appendChild(btn); 
     }
 }
 
 function startQuiz(event) {
-  var index = 0;
+  
   clearInterval(timerInterval);
-  titleBox.textContent = questions[index].title;
-  bodyBox.textContent = "";
-  deleteButtons();
-  createButtons(questions[index].choices, buttons);
-  var timerInterval = setInterval(function() {
+  renderQuestions();
+  if (index === 0) deleteButtons(buttons);
+  else             deleteButtons(questionsBtn);
+  createButtons(questions[index].choices, questionsBtn);
+  timerInterval = setInterval(function() {
       //event.stopPropagation();???
     secondsLeft--;
-    timer.textContent = secondsLeft;
+    renderTimeToPage();
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
     }
 
-  }, 1000);
+  }, 100);
 }
 
-function questionAnswered(event) { 
-    clearInterval(timerInterval);   
-    for (x in box2.children) {
-        if (box2.children[x].firstChild === event.target.firstChild) 
-        {
-            console.log(event);
-        };
-    }
+// function questionAnswered(event) { 
+//     //answer = findAnswer();
+//     //console.log(answer);
     
+//     //console.log(questions[0].answer);
+    
+//     //console.log(checkAnswer(answer, questions[0].answer));
+    
+// }
+
+// function findAnswer() {
+//     //var btn = document.querySelector("#answerBtn");
+//     //console.log(btn);
+//     var element = event.target;
+//     console.log(event);
+    
+//     questionsBtn.querySelectorAll('*').forEach(function(n){
+//         console.log(n.firstChild.data === questions[0].answer)}
+//     )}
+    // for (x in questionsBtn.children) {
+    //     console.log(questionsBtn.children[x].firstChild.data === questions[0].answer);
+    //     console.log(questionsBtn.children[x].firstChild.data);
+    //     console.log(questions[0].answer);
+    //         //box2.children[x].firstChild === event.target.firstChild) 
+    //     // {
+    //     //     clearInterval(timerInterval);
+    //     //     console.log(questionsBtn.children[x].firstChild.data);            
+    //     //     return questionsBtn.children[x].firstChild.data//box2.children[x].firstChild.data;
+    //     // }
+        
+    // }
+
+
+function checkAnswer(event, arrayAnswer) {
+    if (!(event.target.firstChild.data === arrayAnswer)) secondsLeft -= penalty;
+    index++;
+    startQuiz();
+}
+
+function renderScoreInput() {
+    clearInterval(timerInterval);
+    deleteButtons(questionsBtn);
+    renderScore();
 }
 
 startButton.addEventListener("click", startQuiz);
-buttons.addEventListener("click", questionAnswered);
+questionsBtn.addEventListener("click", function() {
+    event.preventDefault();
+    if (index === questions.length - 1) renderScoreInput();
+    else checkAnswer(event, questions[index].answer);
+});
+questionsBtn.addEventListener("submit", function(event) {
+    //event.preventDefault();
+    console.log("hello");
+  });
 
 
